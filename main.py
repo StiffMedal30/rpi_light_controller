d3dc048eb4e8ae6edccf21f3ac46e124d5836715
Christiaan Dotze <christiaan@dotze.co.za>
2021-11-12 07:46:31 +0200

    added status check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import RPi.GPIO as GPIO
 import serial
+import time
 from flask import Flask, redirect
 
 app = Flask(__name__)
@@ -11,33 +11,50 @@ if __name__ == '__main__':
 
 
 
-relay = 7
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(relay, GPIO.OUT)
-
-
-
 @app.route('/light/on')
 def lights_on():
-    line = serialComs(b"on\n")
-    return redirect('/' + str(line))
+    print("request on")
+    line = serialComsWrite(b"on\n")
+    print(line)
+    return redirect('/')
 
 
 @app.route("/light/off")
 def lights_off():
-    line = serialComs(b"off\n")
-    return redirect('/' + str(line))
+    print("request off")
+    line = serialComsWrite(b"off\n")
+    return redirect('/')
 
 
-@app.route("/<line>")
-def lights(line):
-    print("The Light Is Now " + line)
-    return str(line)
+@app.route("/light/status")
+def light_status():
+    line = serialComsRead(b"status\n")
+    if line == "":
+        line = "unknown"
+    
+    print("status:" + line)
+    
+    return redirect("/")
 
-def serialComs(state):
-    ser = serial.Serial('/dev/ttyACM0',9600 , timeout=1)
+
+@app.route("/")
+def lights():
+    return "hello"
+
+
+def serialComsWrite(state):
+    ser = serial.Serial('/dev/ttyAMA0',9600 , timeout=1)
     ser.flush()
     ser.write(state)
+    time.sleep(0.5)
+    line = ser.readline().decode('utf-8').rstrip()
+    return line
+
+def serialComsRead(state):
+    ser = serial.Serial('/dev/ttyAMA0',9600 , timeout=1)
+    ser.flush()
+    ser.write(state)
+    time.sleep(0.5)
     line = ser.readline().decode('utf-8').rstrip()
     return line
 
