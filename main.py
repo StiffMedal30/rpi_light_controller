78c5c4ba5ab56d308ec532107802705a4acb0be8
Christiaan Dotze <christiaan@dotze.co.za>
2022-01-05 09:08:51 +0200

    update

--- main.py
+++ main.py
@@ -1,61 +1,56 @@
 import serial
 import time
-from flask import Flask, redirect
+import requests
+from flask import Flask, redirect, jsonify, url_for
+
 
 app = Flask(__name__)
+URL = 'http://christiaan.ddns.net'
+
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
-    
+
 
 
 
 @app.route('/light/on')
 def lights_on():
-    print("request on")
-    line = serialComsWrite(b"on\n")
-    print(line)
-    return redirect('/')
+    serialComs(b"on\n")
+    return ""
 
 
 @app.route("/light/off")
 def lights_off():
-    print("request off")
-    line = serialComsWrite(b"off\n")
-    return redirect('/')
+    serialComs(b"off\n")
+    return ""
 
 
 @app.route("/light/status")
 def light_status():
-    line = serialComsRead(b"status\n")
-    if line == "":
-        line = "unknown"
-    
-    print("status:" + line)
-    
-    return redirect("/")
+    status = serialComs(b"status\n")
+    print(status)
+    if status == "":
+        status = "unknown"
+    if status == "n":
+        status = "on"
+    if status == "f":
+        status = "off"
+    return lights(status)
 
 
 @app.route("/")
-def lights():
-    return "hello"
+def lights(status):
+    return status
 
 
-def serialComsWrite(state):
+
+
+def serialComs(state):
     ser = serial.Serial('/dev/ttyAMA0',9600 , timeout=1)
     ser.flush()
     ser.write(state)
     time.sleep(0.5)
     line = ser.readline().decode('utf-8').rstrip()
     return line
-
-def serialComsRead(state):
-    ser = serial.Serial('/dev/ttyAMA0',9600 , timeout=1)
-    ser.flush()
-    ser.write(state)
-    time.sleep(0.5)
-    line = ser.readline().decode('utf-8').rstrip()
-    return line
-
-
